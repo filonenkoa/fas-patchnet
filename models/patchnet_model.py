PatchNet model fix for a proper SoftMax ONNX conversion.

models/patchnet_model.py
@@ -33,7 +33,7 @@ class PatchnetModel(BaseModel):
         return loss
     
     def predict(self, descriptor: Tensor) -> Tensor:
-        return F.softmax(self.patch_loss.amsm_loss.s * self.patch_loss.amsm_loss.fc(descriptor), dim=1)
+        return F.softmax(self.patch_loss.amsm_loss.s * self.patch_loss.amsm_loss.fc(descriptor), dim=-1)
     
     @property
     def can_reparameterize(self) -> bool: